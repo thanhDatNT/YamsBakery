422ab260892b17f87492b1a5c8ebdfab8735dfb5
quynhvtn01 <quynhvtn19411@st.uel.edu.vn>
2021-11-01 16:31:56 +0700

    update see review completed

--- app/src/main/java/com/thanhdat/yams/Activities/ProductDetailsActivity.java
+++ app/src/main/java/com/thanhdat/yams/Activities/ProductDetailsActivity.java
@@ -2,15 +2,37 @@ package com.thanhdat.yams.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
 import com.thanhdat.yams.R;
 
 
 public class ProductDetailsActivity extends AppCompatActivity {
 
+    TextView txtSeeReview;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product_details);
+
+        linkViews();
+        addEvent();
+    }
+
+    private void linkViews() {
+        txtSeeReview = findViewById(R.id.txtSeeReview);
+    }
+
+    private void addEvent() {
+        txtSeeReview.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(ProductDetailsActivity.this,SeeReviewActivity.class));
+            }
+        });
     }
 }
