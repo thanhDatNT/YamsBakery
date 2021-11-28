2455b6786d56af09402623e8db90adb5847a4279
minhxxxx <93394922+minhxxxx@users.noreply.github.com>
2021-11-28 20:05:52 +0700

    update feed

--- app/src/main/java/com/thanhdat/yams/Activities/FeedCommentActivity.java
+++ app/src/main/java/com/thanhdat/yams/Activities/FeedCommentActivity.java
@@ -17,6 +17,10 @@ import android.widget.Toast;
 import com.thanhdat.yams.R;
 
 public class FeedCommentActivity extends AppCompatActivity {
+//
+//    private RecyclerView recyclerView;
+//    private CommentAdapter commentAdapter;
+//    private List<Comment> commentList;
 
     EditText addComment;
     ImageView imageProfile;
@@ -32,7 +36,7 @@ public class FeedCommentActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feed_comment);
 
-        Toolbar toolbar = findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbarComment);
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle(null);
 
@@ -65,16 +69,94 @@ public class FeedCommentActivity extends AppCompatActivity {
                 }
             }
         });
+//        recyclerView = findViewById(R.id.recycler_view);
+//        recyclerView.setHasFixedSize(true);
+//        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+//
+//        commentList = new ArrayList<>();
+//        commentAdapter = new CommentAdapter(this, commentList, postId);
+//
+//        recyclerView.setAdapter(commentAdapter);
+//
+//
+//        fUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        getUserImage();
+
+
 
         getComment();
     }
 
     private void getComment() {
 
+//        FirebaseDatabase.getInstance().getReference().child("Comments").child(postId).addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+//                commentList.clear();
+//
+//                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+//                    Comment comment = snapshot.getValue(Comment.class);
+//                    commentList.add(comment);
+//                }
+//
+//                commentAdapter.notifyDataSetChanged();
+//            }
+//
+//            @Override
+//            public void onCancelled(@NonNull DatabaseError databaseError) {
+//
+//            }
+//        });
+
     }
 
     private void putComment() {
 
+//        HashMap<String, Object> map = new HashMap<>();
+//
+//        DatabaseReference ref = FirebaseDatabase.getInstance().getReference().child("Comments").child(postId);
+//
+//        String id = ref.push().getKey();
+//
+//        map.put("id", id);
+//        map.put("comment", addComment.getText().toString());
+//        map.put("publisher", fUser.getUid());
+//
+//        addComment.setText("");
+//
+//        ref.child(id).setValue(map).addOnCompleteListener(new OnCompleteListener<Void>() {
+//            @Override
+//            public void onComplete(@NonNull Task<Void> task) {
+//                if (task.isSuccessful()) {
+//                    Toast.makeText(CommentActivity.this, "Comment added!", Toast.LENGTH_SHORT).show();
+//                } else {
+//                    Toast.makeText(CommentActivity.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+//                }
+//            }
+//        });
+
+    }
+
+    private void getUserImage() {
+
+//        FirebaseDatabase.getInstance().getReference().child("Users").child(fUser.getUid()).addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+//                User user = dataSnapshot.getValue(User.class);
+//                if (user.getImageurl().equals("default")) {
+//                    imageProfile.setImageResource(R.mipmap.ic_launcher);
+//                } else {
+//                    Picasso.get().load(user.getImageurl()).into(imageProfile);
+//                }
+//            }
+//
+//            @Override
+//            public void onCancelled(@NonNull DatabaseError databaseError) {
+//
+//            }
+//        });
+//
     }
 
 }
--- app/src/main/java/com/thanhdat/yams/Activities/OrderDetailActivity.java
+++ app/src/main/java/com/thanhdat/yams/Activities/OrderDetailActivity.java
@@ -1,8 +1,11 @@
 package com.thanhdat.yams.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.View;
 
 import com.thanhdat.yams.R;
 
@@ -12,5 +15,19 @@ public class OrderDetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order_detail);
+
+        Toolbar toolbar = findViewById(R.id.toolbarOrderDetail);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(null);
+
+        Drawable drawable = getResources().getDrawable(R.drawable.ic_back_pink);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeAsUpIndicator(drawable);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
