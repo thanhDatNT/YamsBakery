afb92582ab58a7f2b39aa98e797a55b73f753b5f
minhxxxx <93394922+minhxxxx@users.noreply.github.com>
2021-12-22 18:04:09 +0700

    update cancel order + edit drawable

--- app/src/main/java/com/thanhdat/yams/Activities/NotificationActivity.java
+++ app/src/main/java/com/thanhdat/yams/Activities/NotificationActivity.java
@@ -54,7 +54,7 @@ public class NotificationActivity extends AppCompatActivity {
                 AlertDialog.Builder builder= new AlertDialog.Builder(NotificationActivity.this);
                 builder.setTitle("Xác nhận xóa!");
                 builder.setMessage("Bạn chắc chắn xóa thông báo này?");
-                builder.setIcon(R.mipmap.ic_logo_launcher);
+                builder.setIcon(R.mipmap.ic_launcher);
                 builder.setPositiveButton("Đồng ý", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
@@ -95,7 +95,7 @@ public class NotificationActivity extends AppCompatActivity {
         AlertDialog.Builder builder= new AlertDialog.Builder(NotificationActivity.this);
         builder.setTitle("Xác nhận xóa!");
         builder.setMessage("Bạn chắc chắn xóa tất cả thông báo?");
-        builder.setIcon(R.mipmap.ic_logo_launcher);
+        builder.setIcon(R.mipmap.ic_launcher);
         builder.setPositiveButton("Đồng ý", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
